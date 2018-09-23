75f0bf61312899a385cba299e3495097939a0979
Max Kuzmin <max_kuzmin@outlook.com>
2018-09-23 15:36:26 +0300

    C++

--- FindNumbers/Program.cs
+++ FindNumbers/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,33 +10,57 @@ namespace FindNumbers
     {
         static double toSearch = 10958;
         static long iter = 0;
-        static StreamWriter file;
         static int OpsNum = 9;
         static int OpdNum = 10;
+        static StreamWriter file;
+        static bool stop = false;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Number to search:");
             toSearch = double.Parse(Console.ReadLine());
-
             file = new StreamWriter(File.OpenWrite("output.txt"));
-            Op[] operations = new Op[OpsNum]; //between digits
-            IterateOperations(operations, 0);
 
-            Console.WriteLine("Completed!");
+            //Start indicator
+            Task.Run(() =>
+            {
+                while (!stop)
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Iteration: " + iter);
+                }
+            });
+
+            //Main loop
+            Parallel.For(0, 12, (i) =>
+            {
+                Num[] results = new Num[OpdNum];
+                for (int k = 0; k < OpdNum; k++)
+                    results[k] = new Num(k);
+                int[] order = new int[OpsNum];
+                Op[] operations = new Op[OpsNum]; //between digits
+                bool[] alreadyInOrder = new bool[OpsNum];
+
+                IterateOperations(operations, 0, results, order, alreadyInOrder, (Op)(i % 2), (Op)(i % 6));
+            });
+
             file.Close();
-            Console.ReadKey();
+            Console.WriteLine("Completed!");
+            Console.Out.Flush();
+            Console.ReadLine();
         }
 
-
-        static void IterateOperations(Op[] operations, int opIndex)
+        static void IterateOperations(Op[] operations, int opIndex, Num[] results, int[] order, bool[] alreadyInOrder, Op firstOp = Op.Sum, Op secondOp = Op.Sum)
         {
+            if (stop) return;
+            iter++;
+
             //Check combination
-            int[] order = new int[OpsNum];
-            for (int i = 0; i < OpsNum; i++)
-                order[i] = -1;
-            int orderIndex = 0;
-            IterateBraces(operations, order, orderIndex);
+            int i = 0;
+            for (i = 0; i < OpsNum; i++)
+                alreadyInOrder[i] = false;
+
+            IterateBraces(operations, order, 0, results, alreadyInOrder);
 
             //Go deeper
             if (opIndex == OpsNum)
@@ -47,118 +69,111 @@ namespace FindNumbers
             //Go parallel
             if (opIndex == 0)
             {
-                Parallel.For(0, 6, (i) =>
-                {
-                    Op[] operationsCopy = new Op[OpsNum];
-                    Array.Copy(operations, operationsCopy, OpsNum);
-                    operationsCopy[opIndex] = (Op)i;
-                    IterateOperations(operationsCopy, opIndex + 1);
-                });
+                operations[0] = firstOp;
+                operations[1] = secondOp;
+                IterateOperations(operations, 2, results, order, alreadyInOrder);
             }
             else
             {
-                for (int i = 0; i < 6; i++)
+                for (i = 0; i < 6; i++)
                 {
                     operations[opIndex] = (Op)i;
-                    IterateOperations(operations, opIndex + 1);
+                    IterateOperations(operations, opIndex + 1, results, order, alreadyInOrder);
                 }
             }
         }
 
 
-        static void IterateBraces(Op[] operations, int[] order, int orderIndex)
+        static void IterateBraces(Op[] operations, int[] order, int orderIndex, Num[] results, bool[] alreadyInOrder)
         {
             //Calculate
             if (orderIndex == OpsNum)
             {
-                Calculate(operations, order);
+                Calculate(operations, order, results);
                 return;
             }
 
             //Go deeper
-            for (int i = 0; i < OpsNum; i++)
+            int i = 0;
+            for (i = 0; i < OpsNum; i++)
             {
                 //if concatenation found, go deeper, because it's required computation
-                if (operations[i] == Op.Concat 
-                    && order[0] != i && order[1] != i && order[2] != i && order[3] != i && order[4] != i && order[5] != i && order[6] != i && order[7] != i && order[8] != i)
+                if (operations[i] == Op.Concat && !alreadyInOrder[i])
                 {
                     order[orderIndex] = i;
-                    IterateBraces(operations, order, orderIndex + 1);
+                    alreadyInOrder[i] = true;
+                    IterateBraces(operations, order, orderIndex + 1, results, alreadyInOrder);
                     return;
                 }
             }
-            for (int i = 0; i < OpsNum; i++)
+            for (i = 0; i < OpsNum; i++)
             {
                 //check all other combinations
-                if (order[0] != i && order[1] != i && order[2] != i && order[3] != i && order[4] != i && order[5] != i && order[6] != i && order[7] != i  && order[8] != i)
+                if (!alreadyInOrder[i])
                 {
                     order[orderIndex] = i;
-                    IterateBraces(operations, order, orderIndex + 1);
-                    order[orderIndex] = -1;
+                    alreadyInOrder[i] = true;
+                    IterateBraces(operations, order, orderIndex + 1, results, alreadyInOrder);
+                    alreadyInOrder[i] = false;
                 }
             }
         }
 
-        static void Calculate(Op[] operations, int[] order)
+        static void Calculate(Op[] operations, int[] order, Num[] results)
         {
-            iter++;
-            if (iter % 10000000 == 0)
-            {
-                Console.WriteLine("Iteration: " + iter + ", " + operations.Select(a => a.ToString()).Aggregate((a1, a2) => a1 + a2));
-            }
-
             //Fill array with 1,2,3,4,5,6,7,8,9
-            Num[] results = new Num[OpdNum];
-            for (int i = 0; i < OpdNum; i++)
-                results[i] = new Num(i);
+            int i = 0;
+            for (i = 0; i < OpdNum; i++)
+                results[i].Clear(i);
 
             //For every operation in selected order
             //Calculate it and write result to neighbor cells
-            for (int i = 0; i < OpsNum; i++)
+            for (i = 0; i < OpsNum; i++)
             {
-                double result = 0;
-                double operandOne = results[order[i]].Value;
-                double operandTwo = results[order[i] + 1].Value;
                 switch (operations[order[i]])
                 {
                     case Op.Sum:
-                        result = operandOne + operandTwo;
+                        results[order[i]].Value = results[order[i]].Value + results[order[i] + 1].Value;
                         break;
                     case Op.Sub:
-                        result = operandOne - operandTwo;
+                        results[order[i]].Value = results[order[i]].Value - results[order[i] + 1].Value;
                         break;
                     case Op.Mul:
-                        result = operandOne * operandTwo;
+                        results[order[i]].Value = results[order[i]].Value * results[order[i] + 1].Value;
                         break;
                     case Op.Div:
                         //check for 0 division
-                        if (operandTwo == 0)
+                        if (results[order[i] + 1].Value == 0)
                             return;
-                        result = operandOne / operandTwo;
+                        results[order[i]].Value = results[order[i]].Value / results[order[i] + 1].Value;
                         break;
                     case Op.Concat:
-                        result = operandOne * 10 + operandTwo;
+                        results[order[i]].Value = results[order[i]].Value * 10 + results[order[i] + 1].Value;
                         break;
                     case Op.Pow:
-                        result = Math.Pow(operandOne, operandTwo);
+                        /*if (results[order[i]].Value % 1 != 0 || results[order[i] + 1].Value % 1 != 0)
+                            return;*/
+                        results[order[i]].Value = Math.Pow(results[order[i]].Value, results[order[i] + 1].Value);
                         break;
                 }
 
                 //if too big
-                if (result > int.MaxValue || result < int.MinValue)
-                    return;
-
-                results[order[i]].Value = result;
+                /*if (results[order[i]].Value > int.MaxValue || results[order[i]].Value < int.MinValue)
+                    return;*/
+                    
                 results[order[i] + 1].Ref = results[order[i]];
             }
 
             if (results[order[7]].Value == toSearch)
             {
-                file.WriteLine("Found! Operations: " + operations.Select(a => a.ToString()).Aggregate((a1, a2) => a1 + a2) +
-                    ". Order: " + order.Select(a => a.ToString()).Aggregate((a1, a2) => a1 + a2));
-                Console.WriteLine("Found! Operations: " + operations.Select(a => a.ToString()).Aggregate((a1, a2) => a1 + a2) +
-                    ". Order: " + order.Select(a => a.ToString()).Aggregate((a1, a2) => a1 + a2));
+                //Print result
+                string output = "Number: " + toSearch + ". Operations: " + op(operations[0]) + 1 + op(operations[1]) + 2 + op(operations[2]) + 3 + op(operations[3])
+                    + 4 + op(operations[4]) + 5 + op(operations[5]) + 6 + op(operations[6]) + 7 + op(operations[7]) + 8 + op(operations[8]) +
+                    ". Order: " + order.Select(a => a.ToString()).Aggregate((a1, a2) => a1 + "-" + a2);
+                Console.WriteLine(output);
+                file.WriteLine(output);
                 file.Flush();
+                //stop = true;
             }
         }
 
@@ -172,10 +187,30 @@ namespace FindNumbers
             Concat
         }
 
+        static string op(Op value)
+        {
+            switch (value)
+            {
+                case Op.Sum:
+                    return "+";
+                case Op.Sub:
+                    return "-";
+                case Op.Mul:
+                    return "*";
+                case Op.Div:
+                    return "/";
+                case Op.Pow:
+                    return "^";
+                case Op.Concat:
+                    return "";
+            }
+            return "";
+        }
+
         class Num
         {
             double val;
-            public Num Ref { get; set; }
+            public Num Ref;
             public double Value
             {
                 get
@@ -196,8 +231,13 @@ namespace FindNumbers
 
             public Num(double value)
             {
-                val = value;
+                Clear(value);
+            }
+
+            public void Clear(double value)
+            {
                 Ref = null;
+                val = value;
             }
 
             public override string ToString()
